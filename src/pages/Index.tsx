4ec6e1e28b428d7c5f91c8c6fa877086954bbd5c
[dyad] <git@dyad.sh>
2025-09-12 19:02:35 +0530

    [dyad] Removed floating logo from homepage - wrote 1 file(s), deleted 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,13 +6,11 @@ import Pricing from "@/components/Pricing";
 import Footer from "@/components/Footer";
 import Treatment from "@/components/Treatment";
 import Testimonials from "@/components/Testimonials";
-import FloatingLogo from "@/components/FloatingLogo";
 
 const Index = () => {
   return (
     <div className="flex flex-col min-h-screen bg-gradient-to-b from-background via-secondary/50 to-background">
       <Header />
-      <FloatingLogo />
       <main className="flex-grow">
         <Hero />
         <About />
