b0a8451f57bd767c25db11e9b082255d190c2ddb
[dyad] <git@dyad.sh>
2025-09-12 19:01:21 +0530

    [dyad] Moved the floating logo to the bottom-right corner - wrote 1 file(s)

--- src/components/FloatingLogo.tsx
+++ src/components/FloatingLogo.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const FloatingLogo = () => {
   return (
-    <div className="hidden lg:block fixed top-1/2 -translate-y-1/2 right-8 z-40 transition-transform duration-300 hover:scale-105">
+    <div className="fixed bottom-4 right-4 lg:bottom-8 lg:right-8 z-40 transition-transform duration-300 hover:scale-105">
       <a href="/" aria-label="Back to homepage">
         <img
           src="/poo.jpg"
           alt="Poonguzhali Dental Care Logo"
-          className="w-32 h-32 object-contain rounded-full shadow-lg bg-background/80 p-2"
+          className="w-24 h-24 lg:w-32 lg:h-32 object-contain rounded-full shadow-lg bg-background/80 p-2"
         />
       </a>
     </div>
