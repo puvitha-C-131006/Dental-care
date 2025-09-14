ca186261b0a9eafbb0237e3ea5af15c67cffd19c
[dyad] <git@dyad.sh>
2025-09-14 11:48:39 +0530

    [dyad] Fixed React child error by removing provider - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
@@ -10,22 +9,18 @@ import ContactPage from "./pages/ContactPage";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <>
-    <Toaster />
-    <Sonner />
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <TooltipProvider>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/contact" element={<ContactPage />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </TooltipProvider>
-      </QueryClientProvider>
-    </BrowserRouter>
-  </>
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
+      <Sonner />
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/contact" element={<ContactPage />} />
+        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </QueryClientProvider>
+  </BrowserRouter>
 );
 
 export default App;
