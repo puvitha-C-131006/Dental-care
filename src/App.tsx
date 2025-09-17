54ea0cf931ff7aed86edaac3137b476de67f672d
[dyad] <git@dyad.sh>
2025-09-17 20:34:50 +0530

    [dyad] Set default theme to dark mode - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <TooltipProvider>
         <>
           <Toaster />
